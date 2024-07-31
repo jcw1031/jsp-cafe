6eba270b6c6a2f02e627fb3c8a7a23b42e5f67e1
jcw1031 <jcw001031@gmail.com>
2024-07-31 18:01:44 +0900

    jul 31

---
+++ src/main/java/woopaca/jspcafe/servlet/ReplyServlet.java
@@ -0,0 +1,8 @@
+package woopaca.jspcafe.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/replies/*")
+public class ReplyServlet extends HttpServlet {
+}
